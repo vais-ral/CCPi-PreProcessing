a2162cd06e318d29ca37073405fc35acebad5f11
Srikanth Nagella <srikanthnag@gmail.com>
2017-07-30 00:34:04 +0100

    Update bld.bat

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,9 +1,8 @@
-SET CIL_VERSION 2>Nul | Findstr/I "."
-IF ERRORLEVEL 1  
-(
+IF NOT DEFINED CIL_VERSION (
 ECHO CIL_VERSION Not Defined.
 exit 1
 )
+
 xcopy /e "%RECIPE_DIR%\.." "%SRC_DIR%"
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
