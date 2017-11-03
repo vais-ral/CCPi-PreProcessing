c78e53d4a72f6a956420012e0ada0383e943bb28
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2017-11-03 14:14:59 +0000

    Fixed issues with conda recipe

--- Python/conda-recipe/bld.bat
+++ Python/conda-recipe/bld.bat
@@ -3,6 +3,7 @@ ECHO CIL_VERSION Not Defined.
 exit 1
 )
 
-xcopy /e "%RECIPE_DIR%\.." "%SRC_DIR%"
+xcopy /e "%RECIPE_DIR%\..\.." "%SRC_DIR%"
+cd %SRC_DIR%\Python
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
--- Python/conda-recipe/build.sh
+++ Python/conda-recipe/build.sh
@@ -2,6 +2,6 @@ if [ -z "$CIL_VERSION" ]; then
     echo "Need to set CIL_VERSION"
     exit 1
 fi  
-cp -r "${RECIPE_DIR}/.." ${SRC_DIR}
-cd ${SRC_DIR}
+cp -r "${RECIPE_DIR}/../../" ${SRC_DIR}
+cd ${SRC_DIR}/Python
 $PYTHON setup.py install
