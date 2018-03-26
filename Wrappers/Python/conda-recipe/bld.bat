a7189c880371a64bbe844f9bbade7234266edef3
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2018-03-26 09:09:12 +0100

    conda build error

--- Wrappers/Python/conda-recipe/bld.bat
+++ Wrappers/Python/conda-recipe/bld.bat
@@ -5,5 +5,5 @@ exit 1
 
 xcopy /e "%RECIPE_DIR%\..\..\..\*" "%SRC_DIR%"
 cd %SRC_DIR%\Wrappers\Python
-%PYTHON% setup.py install
+%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
